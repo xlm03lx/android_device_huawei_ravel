655e86bcb4919462f2f0ae14840383394a86f01e
xlm03lx <55821303+xlm03lx@users.noreply.github.com>
2020-01-20 09:14:20 +0800

    ravel: temp remove full_ravel.mk

--- lineage_ravel.mk
+++ lineage_ravel.mk
@@ -14,7 +14,12 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/huawei/ravel/full_ravel.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from charlotte device
+$(call inherit-product, device/huawei/ravel/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
@@ -24,16 +29,6 @@ PRODUCT_DEVICE := ravel
 PRODUCT_BRAND := Huawei
 PRODUCT_MODEL := Honor Note 10
 PRODUCT_MANUFACTURER := Huawei
-
-PRODUCT_GMS_CLIENTID_BASE := android-huawei
-
-#Apps
-PRODUCT_PACKAGES += \
-    Camera2 \
-    Gallery2 \
-    Music \
-    Calendar \
-    DeskClock
 
 # Disable dynamic partition size
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := false
